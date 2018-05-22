d94a97cb94292db98892df41294f29351b5c8237
isaacraynald <isaacoptimus@gmail.com>
2018-05-22 17:20:51 +1000

    add more pages

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Isaac on 18/5/18.
+ */
+// Initialize tooltip component
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+// Initialize popover component
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
